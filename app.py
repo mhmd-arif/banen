552cb6673082789a108d31679f7954e774d2780b
Raden Muhammad Banendra Bagaskara <80038450+Banendra17@users.noreply.github.com>
2024-09-10 15:24:12 +0700

    update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Tambahkan Flask-CORS untuk menangani CORS
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
@@ -8,12 +9,13 @@ from nltk.tokenize import word_tokenize
 import os
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS agar request dari React bisa diterima
 
-# Download necessary NLTK resources
+# Pastikan NLTK resources sudah di-download
 nltk.download('stopwords')
-nltk.download('punkt_tab')  # Tambahkan pengunduhan ini di sini
+nltk.download('punkt')
 
-# Preprocess function
+# Fungsi preprocessing text
 def preprocess_text(text):
     tokens = word_tokenize(text)
     tokens = [word.lower() for word in tokens]
@@ -21,9 +23,9 @@ def preprocess_text(text):
     tokens = [word for word in tokens if word not in stopwords.words('indonesian')]
     return ' '.join(tokens)
 
-# Load data and preprocess
+# Load dataset dan preprocess
 file_path = 'yogyakarta_budaya.csv'
-if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
+if os.path.exists(file_path):
     df = pd.read_csv(file_path)
     df['processed_description'] = df['Description'].apply(preprocess_text)
     vectorizer = TfidfVectorizer()
@@ -31,19 +33,19 @@ if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
 else:
     df = pd.DataFrame()
     vectorizer = None
-    tfidf_matrix = None
 
-# Define the route for recommending museums
+# Route untuk rekomendasi museum
 @app.route('/recommend', methods=['POST'])
 def recommend_museum():
-    if vectorizer is None or tfidf_matrix is None:
-        return jsonify({"error": "Data not available"}), 400
-    
+    if not vectorizer or df.empty:
+        return jsonify({'error': 'Data not found or model not loaded'}), 500
+
     input_desc = request.json['description']
     input_desc_processed = preprocess_text(input_desc)
     input_vector = vectorizer.transform([input_desc_processed])
     similarities = cosine_similarity(input_vector, tfidf_matrix).flatten()
     top_indices = similarities.argsort()[-3:][::-1]
+    
     results = []
     for i in top_indices:
         results.append({
